feceadc86d67e8753b001a78755eea6498e8b9b8
Haochen Zeng <haochen.zeng10@gmail.com>
2024-03-06 22:17:47 -0800

    fix cov computation for beacon_prebuilt

--- dockers/Beacon_prebuilt/convert_to_cov_code.py
+++ dockers/Beacon_prebuilt/convert_to_cov_code.py
@@ -3,7 +3,8 @@ import sys
 code = sys.argv[1]
 
 f = open(code + '.c', 'r')
-cov_code = f.read().replace('abort();','__gcov_flush();\n\tabort();')
+cov_code = f.read().replace('abort();','__gcov_dump();\n\tabort();')
+cov_code = '#include <gcov.h>\n' + cov_code
 f.close()
 
 f_cov = open(code + '_cov.c', 'w')
